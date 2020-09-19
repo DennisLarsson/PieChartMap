599dbdf1eb51eb4dc2b67d0103536c48eda2c8e3
DennisLarsson <43995478+DennisLarsson@users.noreply.github.com>
2020-09-19 10:52:39 +0200

    Update calcAverageQ.R

--- calcAverageQ.R
+++ calcAverageQ.R
@@ -1,6 +1,6 @@
 setwd("/path/to/workDirectory/")    #give the path to the work directory where you files are and/or where you would like to output files to go
 popmap="popmap_spicatum.txt"        #give the name of the popmap if it is in the work directory or the full path if it is somewhere else
-outputname="spicatum"               #give the prefix for all of the output files
+outputname="spicatum_K4"               #give the prefix for all of the output files
 QFile="spicatum_K4_snmf.csv"        #give the name of the Qmatrix file that was created by my sNMF or TESS3 script
 coordinates_file="coordinates.txt"  #give the name of the coordinates file if it is in the work directory or the full path if it is somewhere else
 
@@ -20,12 +20,12 @@ p=2
 while (p<length(coordinates[[1]])){
   AverList=list()
   i=1
-  print(p)
+  #print(p)
   while (pop[[p-1,2]]==pop[[p,2]]){
     AverList[[i]]=Qmatrix[p-1,2:length(Qmatrix[1,])]
     p=p+1
     i=i+1
-    print(p)
+    #print(p)
     if (p > length(coordinates[[1]])){
       break
     }
@@ -55,11 +55,9 @@ while (p<length(coordinates[[1]])){
   while (o <= length(popmean)) {output[o+3]=popmean[o];o=o+1}
   cat(file = paste(outputname,"_popAverage.csv",sep=""),
             x=c(output), append = TRUE,sep = ",",fill = T)
-  print("start")
-  print(AverList)
-  print("end")
-  print(pop[p,2])
+  #print("start")
+  #print(AverList)
+  #print("end")
+  #print(pop[p,2])
   p=p+1
 }
-
-
